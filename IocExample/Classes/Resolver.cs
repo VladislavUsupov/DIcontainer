c5cf8e5c8d95be0a4fc8823e179048dfd7c112f6
VladislavUsupov <vlad74.rus@mail.ru>
2018-05-25 16:08:44 +0500

    First version

---
+++ IocExample/Classes/Resolver.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IocExample.Classes
+{
+    class Resolver
+    {
+        private static Dictionary<Type, Type> dictionaryTypes
+            = new Dictionary<Type, Type>();
+
+        private static Dictionary<Type, object> dictionaryObjects
+            = new Dictionary<Type, object>();
+
+        public static void Bind<T>(object obj)
+        {
+            dictionaryObjects.Add(typeof(T), obj);
+        }
+
+
+
+        public static void Bind<T,V>() where V : T
+        {
+            dictionaryTypes.Add(typeof(T), typeof(V));
+        }
+
+
+
+        public static T Get<T>()
+        {           
+            return (T)Get(typeof(T));
+        }
+
+
+
+        private static object Get(Type type)
+        {
+            if (dictionaryObjects.ContainsKey(type))
+            {
+                return dictionaryObjects[type];
+            }
+            else
+            {
+                type = dictionaryTypes[type];
+                var constructor = Utils.GetSingleConstructor(type);                
+
+                List<Object> resolvedParameters = new List<object>();
+
+                foreach (var parameter in constructor.GetParameters())
+                {
+                    resolvedParameters.Add(Get(parameter.ParameterType));
+                }
+
+                return Utils.CreateInstance(type, resolvedParameters);
+            }
+        }
+    }
+}
---
+++ IocExample/Program.cs
@@ -0,0 +1,60 @@
+﻿using IocExample.Classes;
+using Ninject;
+using System;
+
+namespace IocExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var logger = new ConsoleLogger();
+            //var sqlConnectionFactory = new SqlConnectionFactory("SQL Connection", logger);
+            //var createUserHandler = new CreateUserHandler(new UserService(new QueryExecutor(sqlConnectionFactory), new CommandExecutor(sqlConnectionFactory), new CacheService(logger, new RestClient("API KEY"))), logger);
+
+            //createUserHandler.Handle();
+
+            MainNinject();
+            MainDependencyResolver();
+        }
+
+
+
+        static void MainNinject()
+        {
+            IKernel kernel = new StandardKernel();
+
+            kernel.Bind<ILogger>().To<ConsoleLogger>();
+
+            kernel.Bind<RestClient>()
+                .ToConstructor(k => new RestClient("API_KEY"));
+
+            kernel.Bind<IConnectionFactory>()
+                .ToConstructor(k => new SqlConnectionFactory("SQL Connection", k.Inject<ILogger>()))
+                .InSingletonScope();
+
+            var createUserHandler = kernel.Get<CreateUserHandler>();
+
+            createUserHandler.Handle();
+        }
+
+
+
+        static void MainDependencyResolver()
+        {
+            Resolver.Bind<ILogger, ConsoleLogger>();
+            Resolver.Bind<UserService, UserService>();
+            Resolver.Bind<QueryExecutor, QueryExecutor>();
+            Resolver.Bind<CommandExecutor, CommandExecutor>();
+            Resolver.Bind<CacheService, CacheService>();
+            Resolver.Bind<CreateUserHandler, CreateUserHandler>();
+            Resolver.Bind<IConnectionFactory>(new SqlConnectionFactory("SQL Connection", Resolver.Get<ILogger>()));
+            Resolver.Bind<RestClient>(new RestClient("API_KEY"));
+
+            var createUserHandler = Resolver.Get<CreateUserHandler>();
+            createUserHandler.Handle();
+            Console.ReadKey();
+        }
+    }
+
+}
